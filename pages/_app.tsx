fa88d287de35a46882eec58ec694e7c1c71f1f51
Anay Sharma <anaysharma8791@gmail.com>
2023-04-25 17:44:52 +0530

    add splash screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,25 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const loader = document.getElementById("page-loader");
-      const root = document.querySelector(":root");
-      if (loader) {
-        loader.style.opacity = 1;
-        setTimeout(() => {
-          loader.style.display = "none";
-          root.style.setProperty("--cursor", "none");
-        }, 800);
-      }
-    }
-  }, []);
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			const loader = document.getElementById('page-loader');
+			const root = document.querySelector(':root') as HTMLElement;
 
-  return <Component {...pageProps} />;
+			document.body.style.opacity = '1';
+			document.body.classList.add('rendered');
+
+			if (loader) {
+				loader.classList.add('loaded');
+				setTimeout(() => {
+					loader.style.display = 'none';
+					root.style.setProperty('--cursor', 'none');
+				}, 1000);
+			}
+		}
+	}, []);
+
+	return <Component {...pageProps} />;
 }
