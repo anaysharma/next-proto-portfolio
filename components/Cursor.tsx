60fb766a8528ff56243ece88780fc8f95a1e973f
Anay Sharma <anaysharma8791@gmail.com>
2023-05-10 23:48:06 +0530

    Revert "add cursor animation"
    This reverts commit 95f346f407ea205a26a52c7d6a7b6e91ee541f80.

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -1,21 +1,10 @@
 import useMousePosition from '../hooks/useCursorPosition';
 import useCursorSize from '../hooks/useCursorSize';
 
-import { motion, useMotionValue, useSpring } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export default function Cursor() {
 	const { x, y } = useMousePosition();
-
-	const cursorX = useMotionValue(-100);
-	const cursorY = useMotionValue(-100);
-
-	cursorX.set(x - 5);
-	cursorY.set(y - 5);
-
-	const springConfig = { damping: 30, stiffness: 300 };
-	const cursorXSpring = useSpring(cursorX, springConfig);
-	const cursorYSpring = useSpring(cursorY, springConfig);
-
 	const size = useCursorSize();
 
 	return (
@@ -23,8 +12,8 @@ export default function Cursor() {
 			className="cursor pointer-events-none fixed left-0 top-0 z-50 aspect-square rounded-full bg-transparent backdrop-invert"
 			style={{
 				height: 10,
-				x: cursorXSpring,
-				y: cursorYSpring,
+				x: x - 5,
+				y: y - 5,
 				boxShadow: `0 0 0 ${size}px #e37f6c`,
 			}}
 			transition={{ duration: 0.6 }}
